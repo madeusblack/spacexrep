2497227fb05acf5a05c74d8bfb0fd84224aa56c9
Yayson Olivares <yayson.olivares@gmail.com>
2020-06-28 18:20:59 -0400

    prepared files for production

---
+++ build/precache-manifest.d973827a43925a78610db5cfb49b8afd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a76237c1872130a8b4ea481b81cc9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "329e983f22b9c07d3498",
+    "url": "/static/css/main.ab4810a9.chunk.css"
+  },
+  {
+    "revision": "3be4f31983a6a191ac31",
+    "url": "/static/js/2.c4d27a8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4d27a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329e983f22b9c07d3498",
+    "url": "/static/js/main.a0f5b0c4.chunk.js"
+  },
+  {
+    "revision": "0dff9c0d4024a65fa4d0",
+    "url": "/static/js/runtime-main.e6c947b4.js"
+  }
+]);
