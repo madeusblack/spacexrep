a5156a3946bd882579b45b03214668d94d14c959
Yayson Olivares <yayson.olivares@gmail.com>
2020-07-17 12:37:02 -0400

    added flickr images to launchcards and adapted it to phone view

--- src/components/LaunchCard.jsx
+++ src/components/LaunchCard.jsx
@@ -4,22 +4,33 @@ import YoutubeIframe from './YoutubeIframe';
 import '../assets/styles/components/LaunchCard.scss';
 import paceholder from '../assets/static/paceholder.jpg';
 
-const LaunchCard = ({ title, mission_name, launch_date_utc, links, site_name_long, details }) => {
+const LaunchCard = ({ title, mission_name, launch_date_utc, links, launch_site, details }) => {
   const date = new Date(launch_date_utc);
   const launchDate = `Launch Date: ${date.toDateString()}`;
+  const launchPlace = `Launch Site: ${launch_site.site_name_long}`;
+  const MissionDetails = `About this mission: ${details}`;
   return (
     <div className='launchCard'>
+
       {title && (
         <h2 className='cardTitle'>{title}</h2>
       )}
-      <h4 className='missionName'>{mission_name}</h4>
-      {links.mission_patch ?
-        <img className='missionPatch' src={links.mission_patch} alt='mission patch' /> :
-        <img className='missionPatch' src={paceholder} alt='mission patch' style={{ borderRadius: '52%' }} />}
+      <div className='name_patch'>
+        {links.mission_patch ?
+          <img className='missionPatch' src={links.mission_patch} alt='mission patch' /> :
+          <img className='missionPatch' src={paceholder} alt='mission patch' style={{ borderRadius: '52%' }} />}
+        <h4 className='missionName'>{mission_name}</h4>
+
+      </div>
+      {(links.flickr_images.length > 0) &&
+        <img className='flickr' src={links.flickr_images[0]} alt='' />}
       <h5 className='missionDate'>{launchDate}</h5>
-      <YoutubeIframe source={links.youtube_id} />
-      <h5 className='launchSite'>{site_name_long}</h5>
-      <p className='aboutThisMission'>{details}</p>
+      <h5 className='launchSite'>{launchPlace}</h5>
+
+      <YoutubeIframe className='youtubeIframe' source={links.youtube_id} />
+      <p className='aboutThisMission'>{MissionDetails}</p>
+      {links.wikipedia &&
+        <a className='groupInfo' href={links.wikipedia} target='blank_'>More info about this launch group (Wikipedia)</a>}
     </div>
   );
 };
